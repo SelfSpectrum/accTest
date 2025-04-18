8875450def06d232647083618d931bdb39acada7
PaladynoSM <paladyno.sm@gmail.com>
2025-04-17 21:46:25 -0400

    Sesiones creadas mediante php artisan breeze:install

---
+++ hospital_laravel_test/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome Temporal</title>
+</head>
+<body>
+    <h1>Placeholder</h1>
+    <p>Este archivo es temporal para la instalación de Breeze.</p>
+</body>
+</html>
